b5a1f1b0a05d59897923492f080c907c04e133b0
Tony <nasax2000@gmail.com>
2018-11-13 02:39:43 -0600

    Added comments

--- index.js
+++ index.js
@@ -4,59 +4,64 @@ var url = "https://raw.githubusercontent.com/freeCodeCamp/ProjectReferenceData/m
         bottom: 60,
         left: 60,
         right: 20
-    },
-    width = window.innerWidth - (window.innerWidth * .3),
-    height = window.innerHeight - (window.innerHeight * .3),
-    formatMonth = d3.timeFormat("%B"),
+    }, //margins to use for chart
+    width = window.innerWidth - (window.innerWidth * .3), //get the width of the window and subtract the window width  times 30 percent so the width won't reach all the way to the end
+    height = window.innerHeight - (window.innerHeight * .3), //get the height of the window minus the window height and 30%
+    formatMonth = d3.timeFormat("%B"), //format month
     formatYear = d3.timeFormat('%Y'),
-    parseMonth = d3.timeParse("%m"),
+    parseMonth = d3.timeParse("%m"), //make into actual usuable format
     parseYear = d3.timeParse("%Y"),
-    month = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+    month = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"]; //needed for start and end point for y axis domain
 
 
+//append svg to id chart
 var svg = d3.select('#chart')
     .append("svg")
     .attr("height", height + margin.top + margin.bottom)
     .attr("width", width + margin.left + margin.right)
     .attr('id', "svg");
 
+//append g to svg
 var chart = svg
     .append("g")
 
-const x = d3.scaleTime().range([0, width]);
-const y = d3.scaleBand().range([height, 0]);
+const x = d3.scaleTime().range([0, width]); //scale for years
+const y = d3.scaleBand().range([height, 0]); //scale for months
 
+//grab json data from url
 d3.json(url).then(function (data) {
-    console.log(data);
     const monthly = data.monthlyVariance;
     monthly.forEach(function (d) {
-        d.month = formatMonth(parseMonth(d.month));
+        d.month = formatMonth(parseMonth(d.month)); //format json data into usable information
         d.year = formatYear(parseYear(d.year));
     });
 
-x.domain(d3.extent(monthly, function(d){return new Date(d.year)}));
-y.domain(month);
+    x.domain(d3.extent(monthly, function (d) {
+        return new Date(d.year)
+    }));
+    y.domain(month);
 
     chart.append("g")
         .attr("class", "grid")
-        .attr("transform", "translate(" + margin.left + ", " + height  + ")")
+        .attr("transform", "translate(" + margin.left + ", " + (height + margin.top) + ")")
         .call(d3.axisBottom(x)
-            .tickFormat(d3.timeFormat("%Y")))
+            .tickFormat(d3.timeFormat("%Y"))
+            .ticks(20))
 
     // add the Y gridlines
     chart.append("g")
         .attr("class", "grid")
-        .attr("transform", "translate(" + margin.left +  ", " + 0 + ")")
+        .attr("transform", "translate(" + margin.left + ", " + margin.top + ")")
         .call(d3.axisLeft(y))
 
- /*    chart.append('g')
-        .attr("transform", "translate(0," + (height / 2) + ")")
-        .attr("id", "x-axis")
-        .call(d3.axisBottom(x));
+    /*    chart.append('g')
+           .attr("transform", "translate(0," + (height / 2) + ")")
+           .attr("id", "x-axis")
+           .call(d3.axisBottom(x));
 
-    chart.append("text")
-        .attr("transform", "translate(100, 100)")
-        .style("text-anchor", "middle")
-        .text("YEAR"); */
+       chart.append("text")
+           .attr("transform", "translate(100, 100)")
+           .style("text-anchor", "middle")
+           .text("YEAR"); */
 
 })
