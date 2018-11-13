4e400e3f77b5f7f4f600017994234c0ddf799728
nastex21 <nasax2000@gmail.com>
2018-11-12 22:37:17 -0600

    No real changes.
    Just messing around with the data. Making this branch in case I break the thing.

--- index.js
+++ index.js
@@ -18,13 +18,17 @@ var url = "https://raw.githubusercontent.com/freeCodeCamp/ProjectReferenceData/m
 
 var svg = d3.select('#chart')
     .append("svg")
-    .attr("height", height - (height / 2) + margin.top + margin.bottom)
-    .attr("width", width - (width / 2) + margin.left + margin.right)
+    .attr("height", height - margin.top - margin.bottom)
+    .attr("width", width - margin.left - margin.right)
     .attr('id', "svg");
 
 var chart = svg
     .append("g")
+    .attr("height", height - (height / 2) + margin.top + margin.bottom)
+    .attr("width", width - (width / 2) + margin.left + margin.right)
 
+const x = d3.scaleTime().range([0, width]);
+const y = d3.scaleBand().range([height, 0]);
 
 d3.json(url).then(function (data) {
     console.log(data);
@@ -34,56 +38,45 @@ d3.json(url).then(function (data) {
         d.year = formatYear(parseYear(d.year));
     });
 
-    const xScale = d3.scaleTime()
-        .domain([1753, 2015])
-        .range([0, width]);
+x.domain(d3.extent(monthly, function(d){return d.year}));
+y.domain(month);
 
-    const yScale = d3.scaleBand()
-        .domain(month)
-        .range([height, 0])
-
-    console.log(xScale(2015))
-
-    // gridlines in x axis function
+/*     // gridlines in x axis function
     function make_x_gridlines() {
         return d3.axisBottom(xScale)
             .ticks(263)
-            .tickFormat(d3.timeFormat("%Y"))
+            .tickFormat(d3.timeFormat("%Y"));
     }
 
     // gridlines in y axis function
     function make_y_gridlines() {
         return d3.axisLeft(yScale)
-            .tickFormat(month)
-    }
+            .tickFormat(month);
+    } */
 
     // add the X gridlines
     chart.append("g")
         .attr("class", "grid")
-        .attr("transform", "translate(0," + height + ")")
-        .call(make_x_gridlines()
-            .tickSize(-height, 0, 0)
-            .tickFormat("")
-        )
+        .attr("transform", "translate(0," + height / 2 + ")")
+        .call(d3.axisBottom(x)
+            .tickFormat(d3.timeFormat("%Y")))
 
     // add the Y gridlines
     chart.append("g")
         .attr("class", "grid")
-        .call(make_y_gridlines()
-            .tickSize(-width, 0, 0)
-            .tickFormat("")
-        )
+        .attr("transform", "translate(50, 0)")
+        .call(d3.axisLeft(y)
+            .tickFormat(d3.timeFormat(data.month))
+        );
 
-    chart.append('g')
-        .attr('transform', "translate(0, " + height + ")")
+ /*    chart.append('g')
+        .attr("transform", "translate(0," + (height / 2) + ")")
         .attr("id", "x-axis")
-        .call(d3.axisBottom(xScale));
+        .call(d3.axisBottom(x));
 
     chart.append("text")
-        .attr("transform",
-            "translate(" + (width / 2) + " ," +
-            (height + margin.top) + ")")
+        .attr("transform", "translate(100, 100)")
         .style("text-anchor", "middle")
-        .text("YEAR");
+        .text("YEAR"); */
 
 })
